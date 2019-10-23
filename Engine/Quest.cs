a820278da678f93532da68c8addd966852dfa1f4
Ruchox <ruey01@gmail.com>
2019-10-23 00:30:18 -0400

    Sort of completed. Lots of bugs involving movement however. Also needs to be refactored for cleaner looking code.

--- Engine/Quest.cs
+++ Engine/Quest.cs
@@ -13,7 +13,8 @@ namespace Engine
         public string Description { get; set; }
         public int RewardExperiencePoints { get; set; }
         public int RewardGold { get; set; }
-
+        public Item RewardItem { get; set; }
+        public List<QuestCompletionItem> QuestCompletionItems { get; set; }
         public Quest(int id, string name, string description, int rewardExperiencePoints, int rewardGold)
         {
             ID = id;
@@ -21,6 +22,7 @@ namespace Engine
             Description = description;
             RewardExperiencePoints = rewardExperiencePoints;
             RewardGold = rewardGold;
+            QuestCompletionItems = new List<QuestCompletionItem>();
         }
     }
 }
